f65a5e2c001fd962b04562c62d07b9d5c222966c
paivalucass <lucaspaiva2805@gmail.com>
2024-11-13 10:20:11 -0300

    Fix index column error when calling
    Co-authored-by: Isabela-Moura <Isabela-Moura@users.noreply.github.com>

--- src/labeling_schemas.py
+++ src/labeling_schemas.py
@@ -1,7 +1,7 @@
 TOW_IDS_ATTACK_LABELS = ["C_D", "C_R", "M_F", "F_I", "P_I"]
 NORMAL_KEY = "Normal"
 ABNORMAL_KEY = "Abnormal"
-INDEX_COLUMN = "index"
+INDEX_COLUMN = "Description"
 DESCRIPTION_COLUMN = "Description"
 CLASS_COLUMN = "Class"
 
@@ -26,12 +26,21 @@ def tow_ids_one_class_labeling_schema(y_sequence):
     return seq_y
 
 def tow_ids_multi_class_labeling_schema(y_sequence):
+    
     # Expects y_sequence as a dataframe containing ["Class", "Description"] columns
     # Labeling schema: if there are multiple attacks in the sequence, the label will correspond to the most frequent attack
+    print(y_sequence)
     seq_y = NORMAL_KEY
 
-    indexes = y_sequence[DESCRIPTION_COLUMN].value_counts().sort_values(ascending=False).reset_index()
+    indexes = (
+        y_sequence[DESCRIPTION_COLUMN]
+        .value_counts()
+        .sort_values(ascending=False)
+        .reset_index()
+    )
+        
     indexes_list = list(indexes[INDEX_COLUMN].values)
+    print(indexes_list)
 
     set_attacks = set(TOW_IDS_ATTACK_LABELS)
     set_sequence_indexes = set(indexes_list)
